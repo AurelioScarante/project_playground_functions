ed9da3370180f8627e8d873e0feb92703f072b4f
Aurelio Scarante <aurelioscarante25@gmail.com>
2022-09-08 17:13:14 -0300

    initial commit

---
+++ src/functions.js
@@ -0,0 +1,85 @@
+// Desafio 1
+function compareTrue() {
+  
+}
+
+//Desafio 2
+function calcArea() {
+  
+}
+ 
+// Desafio 3
+function splitSentence() {
+  
+}
+
+//Desafio 4
+function concatName() {
+  
+}
+
+// Desafio 5
+function footballPoints() {
+  
+}
+
+// Desafio 6
+function highestCount() {
+  
+}
+
+// Desafio 7
+function catAndMouse() {
+
+}
+
+// Desafio 8
+function fizzBuzz() {
+ 
+}
+
+// Desafio 9
+function encode() {
+  
+}
+function decode() {
+  
+}
+
+// Desafio 10
+function techList() {
+  
+}
+
+// Desafio 11
+function generatePhoneNumber() {
+  // seu código aqui
+}
+
+// Desafio 12
+function triangleCheck() {
+  // seu código aqui
+}
+
+// Desafio 13
+function hydrate() {
+  // seu código aqui
+}
+
+
+module.exports = {
+  calcArea,
+  catAndMouse,
+  compareTrue,
+  concatName,
+  decode,
+  encode,
+  fizzBuzz,
+  footballPoints,
+  generatePhoneNumber,
+  techList,
+  highestCount,
+  hydrate,
+  splitSentence,
+  triangleCheck,
+}
